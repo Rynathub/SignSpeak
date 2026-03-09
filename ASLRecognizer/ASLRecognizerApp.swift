334dfde98c9dc034360f3b80429969ca73153571
DC <dcexample@gmail.com>
2026-03-09 22:19:39 +0200

    Initial Commit

---
+++ ASLRecognizer/ASLRecognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASLRecognizerApp.swift
+//  ASLRecognizer
+//
+//  Created by Rynat Shakirov on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ASLRecognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASLRecognizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ASLRecognizer
+//
+//  Created by Rynat Shakirov on 09.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
